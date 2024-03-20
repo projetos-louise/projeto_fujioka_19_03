6b8e9b184de07c62f526fb3777657fef6c5f9f55
faculdade <162643414+projetos-louise@users.noreply.github.com>
2024-03-19 23:51:42 -0300

    Update GeneroController.java

--- tecback-fujioka-projeto/src/main/java/br/com/fujideia/iesp/tecback/controller/GeneroController.java
+++ tecback-fujioka-projeto/src/main/java/br/com/fujideia/iesp/tecback/controller/GeneroController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/generos")
+@RequestMapping("/generos")
 public class GeneroController {
     @Autowired
     private GeneroService generoService;
